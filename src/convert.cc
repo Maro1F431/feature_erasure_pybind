0cec7252a7f4c3a8c17f7e4f067dd0904109e358
Mathieu Tammaro <mathieu.tammaro@epita.fr>
2020-07-18 03:14:12 +0200

    second part of conversion

--- src/convert.cc
+++ src/convert.cc
@@ -16,3 +16,20 @@ mln::ndbuffer_image array_to_buffer(py::array_t<float> array)
     return mln::ndbuffer_image::from_buffer((std::byte*) array.data(), mln::sample_type_id::FLOAT
     ,array.ndim(), sizes.data());
 }
+py::array_t<float> buffer_to_array(mln::ndbuffer_image buff)
+{
+    auto vec_size = std::vector<size_t>();
+    for (int i = 0; i < buff.pdim(); i++)
+    {
+        vec_size.push_back(buff.size(i));
+    }
+     return py::array_t<float>(
+        	py::buffer_info(
+	            buff.buffer(),
+		       sizeof(buff.sample_type()), //itemsize
+		       py::format_descriptor<float>::format(),
+		       buff.pdim(), // ndim
+		       vec_size, // shape
+		       std::nullptr // strides
+            );
+}
