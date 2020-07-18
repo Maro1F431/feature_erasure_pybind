89c3a19d4c6101dd06afb5345c96bcb5d3967c0a
Jean-Christophe Antoine <jcantoine@live.fr>
2020-07-18 15:29:15 +0200

    fix : convert : add sprides management
    Signed-off-by: Jean-Christophe Antoine <jcantoine@live.fr>

--- src/convert.cc
+++ src/convert.cc
@@ -11,11 +11,36 @@ mln::ndbuffer_image array_to_buffer(py::array_t<float> array)
     auto sizes = std::vector<int>();
     for (int i = 0; i < array.ndim(); i++)
     {
-      sizes.push_back(array.shape(i));
+        sizes.push_back(array.shape(i));
     }
-    return mln::ndbuffer_image::from_buffer((std::byte*) array.data(), mln::sample_type_id::FLOAT
-    ,array.ndim(), sizes.data());
+    return mln::ndbuffer_image::from_buffer(
+        (std::byte*) array.data(),
+	mln::sample_type_id::FLOAT,
+	array.ndim(), 
+	sizes.data()
+    );
 }
+
+std::vector<int> get_strides(mln::ndbuffer_image& buff)
+{
+    std::vector<int> res;
+    for (int i = 0; i < buff.pdim(); ++i)
+        res.push_back(buff.byte_stride(i));
+
+    for (int i = 0; i < buff.pdim(); ++i)
+    {
+        for (int j = i + 1; j < buff.pdim(); ++j)
+        {
+            res[i] *= buff.size(j);
+	}
+	for (int j = i - 1; j >= 0; --j)
+	{
+            res[i] /= buff.size(j);
+        }
+    }
+    return res;
+}
+
 py::array_t<float> buffer_to_array(mln::ndbuffer_image buff)
 {
     auto vec_size = std::vector<size_t>();
@@ -23,12 +48,14 @@ py::array_t<float> buffer_to_array(mln::ndbuffer_image buff)
     {
         vec_size.push_back(buff.size(i));
     }
-     return py::array_t<float>(
-        	py::buffer_info(
-	            buff.buffer(),
-		       sizeof(buff.sample_type()), //itemsize
-		       py::format_descriptor<float>::format(),
-		       buff.pdim(), // ndim
-		       vec_size.data() // shape
-		       ));
+    return py::array{
+       	py::buffer_info(
+	    buff.buffer(),
+            sizeof(buff.sample_type()), //itemsize
+            py::format_descriptor<float>::format(),
+            buff.pdim(), // ndim
+            vec_size, // shape
+            get_strides(buff) // strides
+        )
+    };
 }
