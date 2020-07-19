9053ac493cd286512a1a2e95f814f303c23f5c37
Mathieu Tammaro <mathieu.tammaro@epita.fr>
2020-07-19 16:58:28 +0200

    fixes on tests and added converting twice in convert.hh header

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -6,7 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-import build.pylene as pylene # He will find the library alone if it has been created
+import pylene as pylene # He will find the library alone if it has been created
 import numpy as np
 
 def conversion_assertion(to_convert):
@@ -26,5 +26,5 @@ def test_three_dim():
     conversion_assertion(np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]))
 
 def test_type_erasure():
-    print(pylene.test())
+    assert pylene.test() == 1
 
